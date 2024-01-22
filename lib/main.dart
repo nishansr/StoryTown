cb2a3712c9fad16905f173a079b5b755a5e34779
Nishan Bishwokarma <nishanbishokarma98@gmail.com>
2024-01-22 08:51:17 +0545

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:chat_app/firebase_options.dart';
+import 'package:chat_app/pages/home_screen.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:chat_app/pages/sign_in.dart';
+import 'package:chat_app/service/auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) {
+        return const MyApp();
+      },
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+        useMaterial3: true,
+      ),
+      home: FutureBuilder(
+        future: AuthMethods().getcurrentUser(),
+        builder: (context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasData) {
+            return const Home();
+          } else {
+            return const SignIn();
+          }
+        },
+      ),
+    );
+  }
+}
